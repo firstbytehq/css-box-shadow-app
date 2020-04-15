425929eae36233dd29857380c60e8563c19cf855
Anju mary paul <anju@firstbytedigital.com>
2020-04-15 09:23:17 +0530

    PR requested change

--- src/components/PreviewContainer.js
+++ src/components/PreviewContainer.js
@@ -68,8 +68,8 @@ const PreviewContainer = ({ shapes, shadowControls }) => {
     let r = parseInt(hexcolor.substr(0,2),16);
     let g = parseInt(hexcolor.substr(2,2),16);
     let b = parseInt(hexcolor.substr(4,2),16);
-    let britness = ((r*299)+(g*587)+(b*114))/1000;
-    const textColor = (britness >= 128) ? 'black' : 'white';
+    let brightness = ((r*299)+(g*587)+(b*114))/1000;
+    const textColor = (brightness >= 128) ? 'black' : 'white';
     setPreviewTextColor(textColor)
   }
 
